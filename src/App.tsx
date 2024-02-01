95ff4051126f5b243c57b39e97301456636e8bcf
Chris Deck <deckchris95@gmail.com>
2024-02-01 15:55:10 -0500

    redirects

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
     >
       <Routes>
         <Route path="/" element={<PublicHub />} />
-        <Route path="/admin/*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Box>
   );
